8e73e12a23dafc8769b2d7d8d317282552d7b8ae
Anton Niklasson <anton@finhack.se>
2020-12-02 18:58:38 +0100

    day 2020-01

--- 2020/01/index.js
+++ 2020/01/index.js
@@ -2,6 +2,46 @@
 
 import { readFile } from '../utils'
 
-const input = readFile('./input')
+const input = readFile('./01/input').split('\n').map(a => Number(a)).sort(comparator)
 
-console.log({ input })
+function comparator(a,b) {
+  return a > b ? 1 : a < b ? -1 : 0;
+}
+
+function sum() {
+  return [...arguments].reduce((s, k) => s + input[k], 0)
+}
+
+function part1() {
+  for(let a = 0; a < input.length - 1; a++) {
+    for(let b = 0; b < input.length - 1; b++) {
+      let c = sum(a, b);
+
+      if (c === 2020) {
+        return input[a] * input[b]
+      }
+    }
+  }
+}
+
+function part2() {
+  for(let a = 1; a < input.length - 1; a++) {
+    for(let b = 1; b < input.length - 1; b++) {
+      for(let c = 1; c < input.length - 1; c++) {
+        let d = sum(a, b, c)
+
+        if (d === 2020) {
+          console.log({
+            a: input[a],
+            b: input[b],
+            c: input[c]
+          })
+
+          return input[a] * input[b] * input[c]
+        }
+      }
+    }
+  }
+}
+
+console.log({ 'part one': part1(), 'part two': part2() })
